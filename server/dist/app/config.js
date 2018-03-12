4f508e7408d94adacd3b17583563ef3bab18b4bd
ddimaria28 <david.dimaria@mapquest.com>
2018-03-12 12:09:20 -0600

    Remove non-repo files

--- server/dist/app/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const config = {
-    port: parseInt(process.env.NODE_PORT, 10) || 3000
-};
-exports.config = config;
-//# sourceMappingURL=config.js.map
--- server/dist/app/database.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Database = require("better-sqlite3");
-exports.db = new Database('loyalty-checkin.db');
-exports.createUsersTable = () => exports.db.exec('CREATE TABLE IF NOT EXISTS users (phone TEXT PRIMARY KEY, firstName TEXT, lastName TEXT, email TEXT)');
-exports.createUserCheckinTable = () => exports.db.exec('CREATE TABLE IF NOT EXISTS user_checkin (phone TEXT, date TEXT)');
-exports.createTables = () => exports.createUsersTable() && exports.createUserCheckinTable();
-//# sourceMappingURL=database.js.map
